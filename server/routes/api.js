d7340d0eff38f9f1083e2fe2292a00d35ff636fe
Luke Chen Shui <chenshuiluke@gmail.com>
2017-08-09 16:12:57 -0500

    Added error handling

--- server/routes/api.js
+++ server/routes/api.js
@@ -25,23 +25,25 @@ api.get("/stories/top", function (req, res) {
     }
   }
   setInterval(function () {
-    try {
+
       var stories = hn.getTopStories();
       var storyArray = [];
       for (var counter = 0; counter <= stories.length; counter++) {
-        var element = stories[counter];
-        if (counter == stories.length) {
-          time = new Date();
-          storyArr = storyArray;
-          return;
+        try {
+          var element = stories[counter];
+          if (counter == stories.length) {
+            time = new Date();
+            storyArr = storyArray;
+            return;
+          }
+          var story = hn.getItem(element);
+          storyArray.push(story);
         }
-        var story = hn.getItem(element);
-        storyArray.push(story);
+        catch (err) {
+          console.log(err);
+        }
       }
-    }
-    catch (err) {
-      console.log(err);
-    }
+
   }, 600000)
 
 });
